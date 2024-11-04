9586a84bbba444a9c6d89013df904bc7adabacaf
Alexander Debus <a.debus@hzdr.de>
2024-11-04 06:04:12 -0500

    Move duplicate code to a new TWTSTight base class.

--- include/picongpu/fields/background/templates/twtstight/BField.hpp
+++ include/picongpu/fields/background/templates/twtstight/BField.hpp
@@ -37,84 +37,10 @@ namespace picongpu
         /* Traveling-wave Thomson scattering laser pulse */
         namespace twtstight
         {
-            class BField
+            class BField : public TWTSTight
             {
             public:
-                using float_T = float_X;
-
-                /** Center of simulation volume in number of cells */
-                PMACC_ALIGN(halfSimSize, DataSpace<simDim>);
-                /** y-position of TWTS coordinate origin inside the simulation coordinates [meter]
-                 *  The other origin coordinates (x and z) default to globally centered values
-                 *  with respect to the simulation volume.
-                 */
-                PMACC_ALIGN(focus_y_SI, float_64 const);
-                /** Laser wavelength [meter] */
-                PMACC_ALIGN(wavelength_SI, float_64 const);
-                /** TWTS laser pulse duration [second] */
-                PMACC_ALIGN(pulselength_SI, float_64 const);
-                /** line focus height of TWTS pulse [meter] */
-                PMACC_ALIGN(w_x_SI, float_64 const);
-                /** TWTS interaction angle
-                 *  Enclosed by the laser propagation direction and the y-axis.
-                 *  For a positive value of the interaction angle, the laser propagation direction
-                 *  points along the y-axis and against the z-axis.
-                 *  That is, for phi = 90 degree the laser propagates in the -z direction.
-                 * [rad]
-                 */
-                PMACC_ALIGN(phi, float_X const);
-                /** Takes value 1.0 for phi > 0 and -1.0 for phi < 0. */
-                PMACC_ALIGN(phiPositive, float_X);
-                /** propagation speed of TWTS laser overlap
-                    normalized to the speed of light. [Default: beta0 = 1.0] */
-                PMACC_ALIGN(beta_0, float_X const);
-                /** If auto_tdelay=FALSE, then a user defined delay is used. [second] */
-                PMACC_ALIGN(tdelay_user_SI, float_64 const);
-                /** Make time step constant accessible to device. */
-                PMACC_ALIGN(dt, float_64 const);
-                /** Make length normalization constant accessible to device. */
-                PMACC_ALIGN(unit_length, float_64 const);
-                /** TWTS laser time delay */
-                PMACC_ALIGN(tdelay, float_64);
-                /** Should the TWTS laser time delay be chosen automatically, such that
-                 * the laser gradually enters the simulation volume? [Default: TRUE]
-                 */
-                PMACC_ALIGN(auto_tdelay, bool const);
-                /** Polarization angle of TWTS laser with respect to x-axis rotated around propagation direction. */
-                PMACC_ALIGN(polAngle, float_X const);
-
-                /** Magnetic field of the TWTS laser
-                 *
-                 * @param focus_y_SI the distance to the laser focus in y-direction [m]
-                 * @param wavelength_SI central wavelength [m]
-                 * @param pulselength_SI sigma of std. gauss for intensity (E^2),
-                 *  pulselength_SI = FWHM_of_Intensity / 2.35482 [seconds (sigma)]
-                 * @param w_x beam waist: distance from the axis where the pulse electric field
-                 *  decreases to its 1/e^2-th part at the focus position of the laser [m]
-                 * @param phi interaction angle between TWTS laser propagation vector and
-                 *  the y-axis [rad, default = 90. * (PI/180.)]
-                 * @param beta_0 propagation speed of overlap normalized to
-                 *  the speed of light [c, default = 1.0]
-                 * @param tdelay_user manual time delay if auto_tdelay is false
-                 * @param auto_tdelay calculate the time delay such that the TWTS pulse is not
-                 *  inside the simulation volume at simulation start timestep = 0 [default = true]
-                 * @param polAngle determines the TWTS laser polarization angle with respect to x-axis around
-                 * propagation direction [rad, default = 0. * (PI/180.)] Normal to laser pulse front tilt plane:
-                 * polAngle = 0.0 * (PI/180.) (linear polarization parallel to x-axis) Parallel to laser pulse front
-                 * tilt plane: polAngle = 90.0 * (PI/180.) (linear polarization parallel to yz-plane)
-                 */
-                HINLINE
-                BField(
-                    float_64 const focus_y_SI,
-                    float_64 const wavelength_SI,
-                    float_64 const pulselength_SI,
-                    float_64 const w_x_SI,
-                    float_X const phi = 90. * (PI / 180.),
-                    float_X const beta_0 = 1.0,
-                    float_64 const tdelay_user_SI = 0.0,
-                    bool const auto_tdelay = true,
-                    float_X const polAngle = 0. * (PI / 180.));
-
+                using TWTSTight::TWTSTight;
 
                 /** Specify your background field B(r,t) here
                  *
@@ -160,66 +86,6 @@ namespace picongpu
                     floatD_X const& cellIdx,
                     pmacc::math::Vector<floatD_X, detail::numComponents> const& extraShifts,
                     float_X const currentStep) const;
-
-                HDINLINE
-                std::tuple<
-                    float_T,
-                    float_T,
-                    float_T,
-                    float_T,
-                    float_T,
-                    float_T,
-                    float_T,
-                    float_T,
-                    float_T,
-                    float_T,
-                    float_T>
-                defineBasicHelperVariables() const;
-
-                HDINLINE
-                std::tuple<float_T, float_T, float_T, float_T> defineMinimalCoordinates(
-                    float3_64 const& pos,
-                    float_64 const time) const;
-
-                HDINLINE
-                std::tuple<float_T, float_T, float_T, float_T, float_T> defineTrigonometryShortcuts(
-                    float_T const absPhi,
-                    float_T const sinPhi) const;
-
-                HDINLINE
-                std::tuple<
-                    alpaka::Complex<float_T>,
-                    float_T,
-                    float_T,
-                    float_T,
-                    float_T,
-                    float_T,
-                    float_T,
-                    float_T,
-                    alpaka::Complex<float_T>,
-                    alpaka::Complex<float_T>,
-                    float_T,
-                    alpaka::Complex<float_T>,
-                    alpaka::Complex<float_T>,
-                    alpaka::Complex<float_T>>
-                defineCommonHelperVariables(
-                    float_T const absPhi,
-                    float_T const sinPhi,
-                    float_T const cosPhi,
-                    float_T const beta0,
-                    float_T const tanAlpha,
-                    float_T const cspeed,
-                    float_T const lambda0,
-                    float_T const omega0,
-                    float_T const tauG,
-                    float_T const w0,
-                    float_T const k,
-                    float_T const x,
-                    float_T const y,
-                    float_T const z,
-                    float_T const t,
-                    float_T const cotPhi,
-                    float_T const sinPhi_2) const;
 
                 /** Calculate the By(r,t) field
                  *
